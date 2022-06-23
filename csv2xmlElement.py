7ae86dad91f21a9523cc0b72386c043001ead262
BinHsu <pcpunkhades@gmail.com>
2022-06-23 08:00:04 +0800

    element has default value by CSV filename

--- csv2xmlElement.py
+++ csv2xmlElement.py
@@ -3,6 +3,7 @@ from xml.etree.ElementTree import ( Element,
                                     tostring,
                                     )
 import argparse
+from pathlib import Path
 
 def toXmlFile():
     with open(csvPathname, 'rt') as r, open(xmlPathname, 'wb') as w:
@@ -22,13 +23,13 @@ def main():
     parser = argparse.ArgumentParser(description='import csv file to xml empty content element with attributes')
     parser.add_argument('--csv', type=str, required=True, help='csv file pathname')
     parser.add_argument('--xml', type=str, required=True, help='xml file pathname')
-    parser.add_argument('--element', type=str, required=True, help='xml element name')
+    parser.add_argument('--element', type=str, required=False, help='xml element name; default will be csv file name')
     parser.add_argument('--ignoring', type=str, required=False, help='csv key which will be ignored')
 
     args = parser.parse_args()
     csvPathname = args.csv
     xmlPathname = args.xml
-    xmlElementName = args.element
+    xmlElementName = args.element if args.element else Path(csvPathname).stem
     csvIgnoring = list(args.ignoring.split(','))
     
     toXmlFile()
